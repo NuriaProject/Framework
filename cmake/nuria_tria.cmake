2a75480648052d09d8964f2d1a14f3ce97735506
Stefan Merettig <stefan-merettig@nuriaproject.org>
2014-10-25 22:23:40 +0200

    nuria_tria.cmake: Use Trias support for multiple input files

--- cmake/nuria_tria.cmake
+++ cmake/nuria_tria.cmake
@@ -12,48 +12,49 @@ function(nuria_tria Target)
   get_target_property(NURIA_TARGET_DEPENDS ${Target} INTERFACE_LINK_LIBRARIES)
   foreach (dependency ${NURIA_TARGET_DEPENDS})
     get_target_property(dependency_inc_dir ${dependency} INTERFACE_INCLUDE_DIRECTORIES)
-	LIST(APPEND NURIA_INC_DIRS ${dependency_inc_dir})
+    LIST(APPEND NURIA_INC_DIRS ${dependency_inc_dir})
   endforeach(dependency)
   LIST(REMOVE_DUPLICATES NURIA_INC_DIRS)
-  # Note that the list may still contain duplicates due to generator statements which will be parsed at a later time.
   
   if(NOT TARGET tria)
     message(WARNING "Tria is not enabled, Target ${Target} won't have runtime information generated.")
     return()
   endif()
   
-  SET(NURIA_TRIA_FILES )
-  SET(TRIA_EXTRA_INCLUDES )
-  
   # Fetch libc/libc++ include directories for win32
   if(WIN32)
+    SET(TRIA_EXTRA_INCLUDES )
     win32_stdlib_include(TRIA_EXTRA_INCLUDES)
+    LIST(APPEND NURIA_INC_DIRS ${TRIA_EXTRA_INCLUDES})
   endif()
   
+  # Prepare include dirs arguments
+  SET(NURIA_INCDIR_ARGUMENT )
+  foreach(Path ${NURIA_INC_DIRS})
+    LIST(APPEND NURIA_INCDIR_ARGUMENT -I${Path})
+  endforeach()
+  
+  # Find header files
+  SET(NURIA_TRIA_INFILES )
   foreach(file ${NURIA_SOURCE_FILES})
     if("${file}" MATCHES "\\.[hH]..$")
-      string(REGEX REPLACE "[^a-zA-Z0-9]" "_" OUTFILE "${file}")
-      string(REGEX REPLACE "(^.*)_[^_]*$" "${CMAKE_CURRENT_BINARY_DIR}/tria_\\1.cpp" OUTFILE "${OUTFILE}")
-      
-      LIST(APPEND NURIA_TRIA_FILES ${OUTFILE})
-      
-      get_property(NURIA_HAS_RULE SOURCE ${OUTFILE} PROPERTY GENERATED SET)
-      
-      if(NOT NURIA_HAS_RULE)
-        add_custom_command(
-            OUTPUT "${OUTFILE}"
-            COMMAND tria -cxx-output=${OUTFILE} ${file} -- ${NURIA_INC_DIRS} ${TRIA_EXTRA_INCLUDES}
-            IMPLICIT_DEPENDS CXX ${file}
-            COMMENT "Running tria on ${file}"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        )
-      endif()
+      LIST(APPEND NURIA_TRIA_INFILES ${file})
     endif()
   endforeach()
   
   # Only add the library if there are matches
-  if (NURIA_TRIA_FILES)
-    add_library("${Target}_tria" STATIC ${NURIA_TRIA_FILES})
+  if(NURIA_TRIA_INFILES)
+    SET(OUTFILE "${CMAKE_CURRENT_BINARY_DIR}/tria_${Target}.cpp")
+    add_custom_command(
+        OUTPUT "${OUTFILE}"
+        COMMAND tria -cxx-output=${OUTFILE} ${NURIA_TRIA_INFILES} ${NURIA_INCDIR_ARGUMENT}
+        DEPENDS ${NURIA_TRIA_INFILES}
+        COMMENT "Running tria for Target ${Target}"
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+    
+    # 
+    add_library("${Target}_tria" STATIC ${OUTFILE})
     
     # Add targets imports to tria target
     get_target_property(NURIA_LINK_INTERFACES ${Target} INTERFACE_LINK_LIBRARIES)
